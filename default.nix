25593b782745a1086b6e0ae5d58a324b17ac13b5
FeepingCreature <default_357-line@yahoo.de>
2019-06-17 23:04:31 +0200

    Add default.nix to build wip test runner

---
+++ default.nix
@@ -0,0 +1,21 @@
+let
+    nixpkgs = import <nixpkgs> {};
+    nixpkgs32 = nixpkgs.pkgsi686Linux;
+in nixpkgs32.stdenv.mkDerivation rec {
+    name = "test";
+    src = ./src;
+    ldc2 = nixpkgs32.callPackage /etc/nixos/overlays/ldc {};
+    dub = nixpkgs32.callPackage /etc/nixos/overlays/dub {};
+    buildInputs = [ ldc2 dub ] ++ (with nixpkgs; [
+        linuxPackages.perf doxygen doxygen_gui
+    ]) ++ (with nixpkgs32; [
+        openssl
+        zlib
+    ]);
+    buildPhase = ''
+        ldc2 -unittest -g -oftest $(find -name \*.d)
+    '';
+    installPhase = ''
+        mv test $out
+    '';
+}
